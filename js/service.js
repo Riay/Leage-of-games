d1f52060bb0df871f5057b1e235e91630e7d8cd5
Yair Silva Mendoza <yairs.mendoza@hotmail.com>
2019-08-29 12:39:29 -0500

    apikey
    apikey change

--- js/service.js
+++ js/service.js
@@ -2,7 +2,7 @@ angular.module('comicApp')
 
     .factory('comicAppService', function($http) {
         function searchComics(nameComic) {
-            var urlSearchComics = 'https://api-comic-vine.herokuapp.com/search/?api_key=5b2bbe51985a5d865b541898bbd424748175b076&format=json&query=' + nameComic
+            var urlSearchComics = 'https://api-comic-vine.herokuapp.com/search/?api_key=c4d827fcba5a6da6369b24fb77588dfbc83c2ab6&format=json&query=' + nameComic
             return $http.get(urlSearchComics)
                 .then(function(response) {
                     var myIssues = response.data.results
@@ -14,7 +14,7 @@ angular.module('comicApp')
         }
 
         function getComicById(id) {
-            var urlSearchComics = 'https://api-comic-vine.herokuapp.com/search/?api_key=5b2bbe51985a5d865b541898bbd424748175b076&format=json&query=' + nameComic
+            var urlSearchComics = 'https://api-comic-vine.herokuapp.com/search/?api_key=c4d827fcba5a6da6369b24fb77588dfbc83c2ab6&format=json&query=' + nameComic
             return $http.get(urlSearchComics)
         }
         return {
